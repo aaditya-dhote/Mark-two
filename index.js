c006074145b1569b8d8ad49b6dd0067755bc4497
Aaditya Dhote <AadityaDhote@outlook.com>
2022-10-05 09:18:57 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 var readlineSync = require('readline-sync');
 var score = 0;
-function play (question,answer)
-{var userAnswer = readlineSync.question(question);
- if(userAnswer===answer)
- {console.log("Great you are right");
- score= score+1;}
-else{console.log("Oops you are wrong");
-    score=score;}
-}  
-  var que = ["Real name of superhero who is also a student? ",
-                 "What superpower does he/she have? ",
-                  "Whats is their costume color? ",
-            "Who gave them their superpower? ",
-            "What is his uncle's name? "];
-var ans = ["Peter parker","Web","Red","Spider","Ben"];
-for (var i=0;i < que.length;i=i+1)
-play(que[i],ans[i]);
+function play(question, answer) {
+  var userAnswer = readlineSync.question(question);
+  if (userAnswer === answer) {
+    console.log("Great you are right");
+    score = score + 1;
+    console.log(score)
+  }
+  else {
+    console.log("Oops you are wrong");
+    score = score;
+    console.log(score)
+  }
+}
+var que = ["Real name of superhero who is also a student? ",
+  "What superpower does he/she have? ",
+  "Whats is their costume color? ",
+  "Who gave them their superpower? ",
+  "What is his uncle's name? "];
+var ans = ["Peter parker", "Web", "Red", "Spider", "Ben"];
+for (var i = 0; i < que.length; i = i + 1)
+  play(que[i], ans[i]);
 console.log("Your final score: " + score)
+console.log("Highest score is 5")
